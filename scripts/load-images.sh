6866282f2eecc0e9e7e80177aade4e7580fbd590
aromanov <aromanov@inno.tech>
2023-01-16 00:09:48 +0400

    migrate to helm charts

--- scripts/load-images.sh
+++ scripts/load-images.sh
@@ -3,11 +3,12 @@
 version=${1:-v2.0}
 
 images=(
-  "postgres:13"
+  "postgres:15"
   "romanowalex/backend-todo-list:$version"
   "influxdb:1.8.4"
   "grafana/grafana:8.3.4"
-  "prom/prometheus:v2.6.0"
+  "prom/prometheus:v2.40.0"
+  "prom/node-exporter:v1.5.0"
   "registry.k8s.io/kube-state-metrics/kube-state-metrics:v2.6.0"
 )
 
